01e927c52ab2c5b04a81bc906e3c38ec3978720a
y.ye <yanjie.jw@163.com>
2015-09-16 09:13:57 +0800

    第一次上传

---
+++ GetSpeedChart/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace GetSpeedChart
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SC_Main = new System.Windows.Forms.SplitContainer();
+            this.PV_Chart = new OxyPlot.WindowsForms.PlotView();
+            this.CB_IP = new System.Windows.Forms.ComboBox();
+            this.ll_ip = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CB_App = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NUD_Time = new System.Windows.Forms.NumericUpDown();
+            this.BT_Login = new System.Windows.Forms.Button();
+            this.BT_Start = new System.Windows.Forms.Button();
+            this.BT_Pause = new System.Windows.Forms.Button();
+            this.RD_MoveID = new System.Windows.Forms.RadioButton();
+            this.RD_Time = new System.Windows.Forms.RadioButton();
+            this.GB_Setting = new System.Windows.Forms.GroupBox();
+            this.GB_Analysis = new System.Windows.Forms.GroupBox();
+            this.CLB_Item = new System.Windows.Forms.CheckedListBox();
+            this.timer_Refresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.SC_Main)).BeginInit();
+            this.SC_Main.Panel1.SuspendLayout();
+            this.SC_Main.Panel2.SuspendLayout();
+            this.SC_Main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Time)).BeginInit();
+            this.GB_Setting.SuspendLayout();
+            this.GB_Analysis.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SC_Main
+            // 
+            this.SC_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SC_Main.Location = new System.Drawing.Point(0, 0);
+            this.SC_Main.Name = "SC_Main";
+            // 
+            // SC_Main.Panel1
+            // 
+            this.SC_Main.Panel1.Controls.Add(this.GB_Analysis);
+            this.SC_Main.Panel1.Controls.Add(this.BT_Pause);
+            this.SC_Main.Panel1.Controls.Add(this.BT_Start);
+            this.SC_Main.Panel1.Controls.Add(this.BT_Login);
+            this.SC_Main.Panel1.Controls.Add(this.GB_Setting);
+            // 
+            // SC_Main.Panel2
+            // 
+            this.SC_Main.Panel2.Controls.Add(this.PV_Chart);
+            this.SC_Main.Size = new System.Drawing.Size(830, 547);
+            this.SC_Main.SplitterDistance = 276;
+            this.SC_Main.TabIndex = 0;
+            // 
+            // PV_Chart
+            // 
+            this.PV_Chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PV_Chart.Location = new System.Drawing.Point(0, 0);
+            this.PV_Chart.Name = "PV_Chart";
+            this.PV_Chart.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.PV_Chart.Size = new System.Drawing.Size(550, 547);
+            this.PV_Chart.TabIndex = 0;
+            this.PV_Chart.Text = "plotView1";
+            this.PV_Chart.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.PV_Chart.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.PV_Chart.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // CB_IP
+            // 
+            this.CB_IP.FormattingEnabled = true;
+            this.CB_IP.Items.AddRange(new object[] {
+            "127.0.0.1",
+            "192.168.0.254",
+            "172.31.0.1"});
+            this.CB_IP.Location = new System.Drawing.Point(93, 24);
+            this.CB_IP.Name = "CB_IP";
+            this.CB_IP.Size = new System.Drawing.Size(134, 21);
+            this.CB_IP.TabIndex = 0;
+            this.CB_IP.Text = "127.0.0.1";
+            // 
+            // ll_ip
+            // 
+            this.ll_ip.AutoSize = true;
+            this.ll_ip.Location = new System.Drawing.Point(18, 29);
+            this.ll_ip.Name = "ll_ip";
+            this.ll_ip.Size = new System.Drawing.Size(47, 13);
+            this.ll_ip.TabIndex = 1;
+            this.ll_ip.Text = "CS8C IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "应用程序名";
+            // 
+            // CB_App
+            // 
+            this.CB_App.FormattingEnabled = true;
+            this.CB_App.Items.AddRange(new object[] {
+            "DataBase",
+            "LasMAN"});
+            this.CB_App.Location = new System.Drawing.Point(93, 69);
+            this.CB_App.Name = "CB_App";
+            this.CB_App.Size = new System.Drawing.Size(134, 21);
+            this.CB_App.TabIndex = 2;
+            this.CB_App.Text = "DataBase";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "轮询时间(秒)";
+            // 
+            // NUD_Time
+            // 
+            this.NUD_Time.DecimalPlaces = 1;
+            this.NUD_Time.Location = new System.Drawing.Point(93, 112);
+            this.NUD_Time.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NUD_Time.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NUD_Time.Name = "NUD_Time";
+            this.NUD_Time.Size = new System.Drawing.Size(134, 20);
+            this.NUD_Time.TabIndex = 5;
+            this.NUD_Time.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // BT_Login
+            // 
+            this.BT_Login.Location = new System.Drawing.Point(58, 197);
+            this.BT_Login.Name = "BT_Login";
+            this.BT_Login.Size = new System.Drawing.Size(128, 36);
+            this.BT_Login.TabIndex = 6;
+            this.BT_Login.Text = "登录";
+            this.BT_Login.UseVisualStyleBackColor = true;
+            this.BT_Login.Click += new System.EventHandler(this.BT_Login_Click);
+            // 
+            // BT_Start
+            // 
+            this.BT_Start.Enabled = false;
+            this.BT_Start.Location = new System.Drawing.Point(58, 252);
+            this.BT_Start.Name = "BT_Start";
+            this.BT_Start.Size = new System.Drawing.Size(128, 36);
+            this.BT_Start.TabIndex = 7;
+            this.BT_Start.Text = "开始";
+            this.BT_Start.UseVisualStyleBackColor = true;
+            this.BT_Start.Click += new System.EventHandler(this.BT_Start_Click);
+            // 
+            // BT_Pause
+            // 
+            this.BT_Pause.Enabled = false;
+            this.BT_Pause.Location = new System.Drawing.Point(58, 309);
+            this.BT_Pause.Name = "BT_Pause";
+            this.BT_Pause.Size = new System.Drawing.Size(128, 36);
+            this.BT_Pause.TabIndex = 8;
+            this.BT_Pause.Text = "暂停";
+            this.BT_Pause.UseVisualStyleBackColor = true;
+            this.BT_Pause.Click += new System.EventHandler(this.BT_Pause_Click);
+            // 
+            // RD_MoveID
+            // 
+            this.RD_MoveID.AutoSize = true;
+            this.RD_MoveID.Location = new System.Drawing.Point(76, 149);
+            this.RD_MoveID.Name = "RD_MoveID";
+            this.RD_MoveID.Size = new System.Drawing.Size(63, 17);
+            this.RD_MoveID.TabIndex = 9;
+            this.RD_MoveID.Text = "MoveID";
+            this.RD_MoveID.UseVisualStyleBackColor = true;
+            // 
+            // RD_Time
+            // 
+            this.RD_Time.AutoSize = true;
+            this.RD_Time.Checked = true;
+            this.RD_Time.Location = new System.Drawing.Point(21, 148);
+            this.RD_Time.Name = "RD_Time";
+            this.RD_Time.Size = new System.Drawing.Size(49, 17);
+            this.RD_Time.TabIndex = 10;
+            this.RD_Time.TabStop = true;
+            this.RD_Time.Text = "时间";
+            this.RD_Time.UseVisualStyleBackColor = true;
+            // 
+            // GB_Setting
+            // 
+            this.GB_Setting.Controls.Add(this.RD_Time);
+            this.GB_Setting.Controls.Add(this.CB_IP);
+            this.GB_Setting.Controls.Add(this.RD_MoveID);
+            this.GB_Setting.Controls.Add(this.ll_ip);
+            this.GB_Setting.Controls.Add(this.CB_App);
+            this.GB_Setting.Controls.Add(this.label2);
+            this.GB_Setting.Controls.Add(this.label3);
+            this.GB_Setting.Controls.Add(this.NUD_Time);
+            this.GB_Setting.Location = new System.Drawing.Point(13, 2);
+            this.GB_Setting.Name = "GB_Setting";
+            this.GB_Setting.Size = new System.Drawing.Size(251, 180);
+            this.GB_Setting.TabIndex = 11;
+            this.GB_Setting.TabStop = false;
+            this.GB_Setting.Text = "配置";
+            // 
+            // GB_Analysis
+            // 
+            this.GB_Analysis.Controls.Add(this.CLB_Item);
+            this.GB_Analysis.Location = new System.Drawing.Point(13, 351);
+            this.GB_Analysis.Name = "GB_Analysis";
+            this.GB_Analysis.Size = new System.Drawing.Size(251, 175);
+            this.GB_Analysis.TabIndex = 12;
+            this.GB_Analysis.TabStop = false;
+            this.GB_Analysis.Text = "分析";
+            // 
+            // CLB_Item
+            // 
+            this.CLB_Item.FormattingEnabled = true;
+            this.CLB_Item.Location = new System.Drawing.Point(21, 20);
+            this.CLB_Item.Name = "CLB_Item";
+            this.CLB_Item.Size = new System.Drawing.Size(206, 139);
+            this.CLB_Item.TabIndex = 0;
+            this.CLB_Item.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CLB_Item_ItemCheck);
+            this.CLB_Item.SelectedIndexChanged += new System.EventHandler(this.CLB_Item_SelectedIndexChanged);
+            // 
+            // timer_Refresh
+            // 
+            this.timer_Refresh.Enabled = true;
+            this.timer_Refresh.Interval = 1000;
+            this.timer_Refresh.Tick += new System.EventHandler(this.timer_Refresh_Tick);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 547);
+            this.Controls.Add(this.SC_Main);
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Staubli机器人末端移动速度";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Main_FormClosed);
+            this.SC_Main.Panel1.ResumeLayout(false);
+            this.SC_Main.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SC_Main)).EndInit();
+            this.SC_Main.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Time)).EndInit();
+            this.GB_Setting.ResumeLayout(false);
+            this.GB_Setting.PerformLayout();
+            this.GB_Analysis.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SC_Main;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CB_App;
+        private System.Windows.Forms.Label ll_ip;
+        private System.Windows.Forms.ComboBox CB_IP;
+        private OxyPlot.WindowsForms.PlotView PV_Chart;
+        private System.Windows.Forms.Button BT_Start;
+        private System.Windows.Forms.Button BT_Login;
+        private System.Windows.Forms.NumericUpDown NUD_Time;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BT_Pause;
+        private System.Windows.Forms.RadioButton RD_Time;
+        private System.Windows.Forms.RadioButton RD_MoveID;
+        private System.Windows.Forms.GroupBox GB_Analysis;
+        private System.Windows.Forms.CheckedListBox CLB_Item;
+        private System.Windows.Forms.GroupBox GB_Setting;
+        private System.Windows.Forms.Timer timer_Refresh;
+    }
+}
+
---
+++ GetSpeedChart/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RobotKit;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GetSpeedChart
+{
+    public partial class Form_Main : Form
+    {
+        SoapClient sc = null;
+        Thread tr = null;
+        bool bExit = false;
+        double[,] speeddata = new double[1000, 2];
+        int ReadCount = 0;
+        int ChartCount = 0;
+
+        bool Pause;
+
+        int LineCount = 0;
+
+        bool GetDataByTime;
+
+        int polltime;
+        PlotModel pm = new PlotModel
+        {
+            Title = "Staubli机器人末端移动速度",
+            // Subtitle = "Example using the FunctionSeries",
+            PlotType = PlotType.Cartesian,
+            Background = OxyColors.White
+        };
+        public Form_Main()
+        {
+            InitializeComponent();
+
+            // PV_Chart.Model = pm;
+
+        }
+
+        private void BT_Login_Click(object sender, EventArgs e)
+        {
+
+            if (CB_IP.Text == "")
+            {
+                MessageBox.Show("请输入CS8C IP 地址");
+                return;
+            }
+
+            if (CB_App.Text == "")
+            {
+                MessageBox.Show("请输入下位机加载的应用程序名");
+                return;
+            }
+
+            sc = new SoapClient(CB_IP.Text.Trim());
+            if (!sc.PingNet())
+            {
+
+                MessageBox.Show("网络不通，请检查IP地址是否正确或者网络链接");
+                return;
+
+            }
+            if (!sc.Login())
+            {
+
+                MessageBox.Show("Soap登录失败");
+                return;
+
+            }
+            sc.SoapGetAppname = @"Disk://" + CB_App.Text + "/" + CB_App.Text + ".pjx";
+
+            byte[] data = sc.GetAppdata();
+
+            if (!RobotKit.StaubliXML.IsGlobelData("nRobotSpeed", data))
+            {
+
+                MessageBox.Show("应用程序中 nRobotSpeed变量不存在");
+                return;
+            }
+
+            GetDataByTime = RD_Time.Checked;
+            if (!GetDataByTime)
+            {
+                if (!RobotKit.StaubliXML.IsGlobelData("nID", data))
+                {
+
+                    MessageBox.Show("应用程序中nID变量不存在");
+                    return;
+                }
+
+
+            }
+
+            MessageBox.Show("登录成功");
+            BT_Start.Enabled = true;
+            GB_Setting.Enabled = false;
+
+
+
+        }
+
+
+
+
+
+
+        private void Poll()
+        {
+            double nidbuff = double.MaxValue;
+            DateTime dt = DateTime.Now;
+            while (!bExit)
+            {
+
+                if (ReadCount - ChartCount < 99 && !Pause)
+                {
+                    byte[] data = sc.GetAppdata();
+
+                    double nid = (DateTime.Now - dt).TotalSeconds;
+                    if (!GetDataByTime)
+                    {
+
+                        nid = StaubliXML.GetXMLDouble("nID", StaubliXML.Byte2xd(data));
+
+                    }
+                    double nSpeed = StaubliXML.GetXMLDouble("nRobotSpeed", StaubliXML.Byte2xd(data));
+
+                    if (nid != nidbuff && nid > 0)
+                    {
+                        nidbuff = nid;
+                    
+                        speeddata[ReadCount % 100, 0] = nid;
+                        speeddata[ReadCount % 100, 1] = nSpeed;
+                        ReadCount++;
+
+                    }
+
+
+                }
+                System.Threading.Thread.Sleep(polltime);
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+        private void BT_Start_Click(object sender, EventArgs e)
+        {
+
+            if (BT_Start.Text == "开始")
+            {
+                if (tr != null && tr.IsAlive)
+                {
+
+                    tr.Abort();
+                }
+                polltime = (int)NUD_Time.Value * 1000;
+                bExit = false;
+                tr = new Thread(new ThreadStart(Poll));
+                tr.Start();
+
+                BT_Start.Text = "终止";
+                BT_Pause.Enabled = true;
+                pm.Series.Clear();
+                Task.Factory.StartNew(() =>
+                {
+                    double nIDbuff = double.MaxValue;
+                    LineSeries ls = new LineSeries();
+                    int LineIndex = 0;
+                    var rd = new Random();
+
+               
+                    while (!bExit)
+                    {
+                        if (ReadCount > ChartCount)
+                        {
+
+                            for (int i = ChartCount; i < ReadCount; i++)
+                            {
+                                if (speeddata[i % 100, 0] < nIDbuff)
+                                {
+                                    byte[] clr = new byte[3];
+                                    rd.NextBytes(clr);
+
+
+                                    ls = new LineSeries("轨迹" + (LineIndex + 1).ToString());
+                                    ls.Color = OxyColor.FromRgb(clr[0], clr[1], clr[2]); ;
+
+                                    pm.Series.Add(ls);
+
+                                    LineIndex++;
+                                }
+                                nIDbuff = speeddata[i % 100, 0];
+                                ls.Points.Add(new DataPoint(speeddata[i % 100, 0], speeddata[i % 100, 1]));
+                                ChartCount++;
+
+
+                            }
+                            PV_Chart.Model = pm;
+
+                            PV_Chart.Model.InvalidatePlot(true);
+                        }
+
+
+
+                        Thread.Sleep(500);
+                    }
+                });
+
+
+
+            }
+            else
+            {
+                bExit = true;
+                Thread.SpinWait(100);
+                if (tr != null && tr.IsAlive)
+                {
+
+                    tr.Abort();
+                }
+
+
+                BT_Start.Text = "开始";
+                BT_Pause.Enabled = false;
+            }
+
+        }
+
+        private void Form_Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            bExit = true;
+            if (tr != null && tr.IsAlive)
+            {
+
+                tr.Abort();
+            }
+
+        }
+
+        private void BT_Pause_Click(object sender, EventArgs e)
+        {
+            if (BT_Pause.Text == "暂停")
+            {
+                BT_Pause.Text = "继续";
+                Pause = true;
+
+            }
+            else
+            {
+
+                BT_Pause.Text = "继续";
+                Pause = false;
+
+            }
+
+
+        }
+
+        private void timer_Refresh_Tick(object sender, EventArgs e)
+        {
+            if (pm != null && pm.Series.Count != LineCount)
+            {
+
+                LineCount = pm.Series.Count;
+                string[] name = new string[LineCount];
+                for(int i=0;i<LineCount;i++)
+                {
+                    name[i] = pm.Series[i].Title;
+
+                }
+                if(LineCount<CLB_Item.Items.Count)
+                {
+                    CLB_Item.Items.Clear();
+                    CLB_Item.Items.AddRange(name);
+                    for(int i=0;i<name.Length;i++)
+                    {
+                        CLB_Item.SetItemChecked(i,true);
+                    }
+
+                }
+                else
+                {
+
+                    for(int i=CLB_Item.Items.Count;i<LineCount;i++)
+                    {
+
+                        CLB_Item.Items.Add(name[i]);
+                        CLB_Item.SetItemChecked(i, true);
+                    }
+
+                }
+
+
+            }
+        }
+
+        private void CLB_Item_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+
+        }
+
+        private void CLB_Item_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CLB_Item.Items.Count <= 0 || pm == null || pm.Series.Count < 1 || pm.Series.Count != CLB_Item.Items.Count)
+
+            {
+                return;
+            }
+            for (int i = 0; i < CLB_Item.Items.Count; i++)
+            {
+
+                pm.Series[i].IsVisible = CLB_Item.GetItemChecked(i);
+
+            }
+            PV_Chart.Model.InvalidatePlot(true);
+        }
+    }
+}
